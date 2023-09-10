a6eea73526066013fa3bd20cfb463c37fb9bd769
Akinz-bobo <adeyemoak1997@gmail.com>
2023-09-10 15:03:40 +0100

    task-one

---
+++ src/routes/requirement.ts
@@ -0,0 +1,21 @@
+import { Router } from "express"
+import data from "../data/data"
+const router = Router()
+
+router.get('/',(req,res,next)=>{
+    // get the query params
+    const {slack_name, track} = req.query as {slack_name:string, track:string}; 
+
+    // check if query parameters exist in data;
+    if(data['slack_name']===slack_name && data['track']===track){
+        res.status(200).json({
+            ...data,
+        })
+    }else{
+        res.status(404).json({
+           message:`could not find user with that parameters, consider using ${data['slack_name']} & ${data['track']}` 
+        }) 
+    }
+})
+
+export default router
